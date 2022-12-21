72c2ed92b10542fb8685a3f2477d8ff705dbd9d1
Daniel Jacon <danielcamillo2020@gmail.com>
2022-12-21 18:11:16 -0300

    página candidatos/curriculo

--- src/pages/Curriculo/index.tsx
+++ src/pages/Curriculo/index.tsx
@@ -1,50 +1,62 @@
-import { Button, Chip, Grid } from "@mui/material";
+import { Button, ButtonGroup, Chip, Grid, Menu, MenuItem } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import React from "react";
+import { objeto } from "shared/utils/states";
 import { CurriculoContainer } from "../../components/CurriculoContainer";
 
 export const Curriculo = () => {
-  const objeto = {
-    nome: "João da Silva",
-    email: "joao@email.com",
-    rg: "123456789",
-    cpf: "123.456.789-00",
-    telefone: "11 99999-9999",
-    estado: "SP",
-    cidade: "São Paulo",
-    dataNascimento: "01/01/2000",
-    neurodiversidade: "Não informado",
-    matriculado: true,
-    turno: "MANHA",
-    instituicao: "Universidade XYZ",
-    curso: "Ciência da Computação",
-    ingles: "Intermediário",
-    espanhol: "Básico",
-    orientacao: "Desenvolvimento de software",
-    genero: "Masculino",
-    trilhas: ["Front-end", "Back-end", "Mobile"],
-    desafiosBoolean: true,
-    problemasBoolean: false,
-    reconhecimentoBoolean: true,
-    altruismoBoolean: true,
-    provaBoolean: false,
-    efetivacaoBoolean: true,
-    disponibilidadeBoolean: true,
-    github: "joaosilva",
-    linkedin: "joaosilva",
-    curriculo: null,
-    lgpdBoolean: true,
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   return (
-    <Grid container spacing={2} sx={{
-      pb: 2,
-    }}>
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        pb: 2,
+      }}
+    >
+      <Grid
+        xs={12}
+        sx={{
+          px: 2,
+          mt: "6px",
+        }}
+      >
+        <Button
+          id="candidatos-avaliar"
+          variant="contained"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          Avaliar
+        </Button>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <MenuItem onClick={handleClose}>Apto</MenuItem>
+          <MenuItem onClick={handleClose}>Não apto</MenuItem>
+        </Menu>
+      </Grid>
       <Grid
         item
         xs={12}
-        md={6}
-        sx={{ height: "calc(100vh - 68px)", width: "100%" }}
+        md={12}
+        sx={{ height: "calc(100vh - 150px)", width: "100%" }}
       >
         <CurriculoContainer resposta={objeto} />
       </Grid>

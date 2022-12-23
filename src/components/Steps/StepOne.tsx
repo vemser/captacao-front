d42af1a604b17980198a78a69f8b42de0665be79
Gicelly Oliveira <gicelly.oliveira@gmail.com>
2022-12-23 15:52:34 -0300

    máscara cpf e telefone, validação de rg e mensagens de erro

--- src/components/Steps/StepOne.tsx
+++ src/components/Steps/StepOne.tsx
@@ -2,187 +2,223 @@ import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import {
-  TextField,
-  Button,
-  Grid,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-  Tooltip,
-  CircularProgress,
+	TextField,
+	Button,
+	Grid,
+	FormControl,
+	InputLabel,
+	Select,
+	MenuItem,
+	Tooltip,
+	CircularProgress,
 } from "@mui/material";
 import { FormGrid } from "../FormGrid";
 import {
-  nextStep,
-  changeData,
+	nextStep,
+	changeData,
 } from "../../shared/features/subscription/stepsSlice";
 import { useDispatch } from "react-redux";
 import { IFormQuery, SubscribeData } from "shared/interfaces";
 import { estadosBrasileiros } from "shared/utils/states";
 import { stepOneSchema } from "shared/schemas/subscription";
 import { useGetInputsQuery } from "shared/features/api/subscription/formSlice";
+import InputMask from "react-input-mask";
+import { cpf as cpfTest } from "cpf-cnpj-validator";
 
 export const StepOne: React.FC = () => {
-  const [neurodiversidade, setNeurodiversidade] = useState("F");
+	const [neurodiversidade, setNeurodiversidade] = useState("F");
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<SubscribeData>({
-    resolver: yupResolver(stepOneSchema),
-  });
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<SubscribeData>({
+		resolver: yupResolver(stepOneSchema),
+	});
 
-  const dispatch = useDispatch();
-  const { data } = useGetInputsQuery();
-  const formulario = data?.data.formulario;
+	const dispatch = useDispatch();
+	const { data } = useGetInputsQuery();
+	const formulario = data?.data.formulario;
 
-  const onSubmit = (data: SubscribeData) => {
-    dispatch(nextStep());
-    dispatch(changeData(data));
-  };
+	const { cpf } = watch();
+	const checkCpf = cpfTest.isValid(cpf);
 
-  const FormName: React.FC<IFormQuery> = ({ nome }) => {
-    return <>{nome ? nome : <CircularProgress size={22} />}</>;
-  };
+	const onSubmit = (data: SubscribeData) => {
+		data.telefone = data.telefone.replace(/[^0-9]/g, "");
+		dispatch(nextStep());
+		dispatch(changeData(data));
+	};
 
-  return (
-    <FormGrid onSubmit={handleSubmit(onSubmit)}>
-      <Grid item xs={12}>
-        <TextField
-          fullWidth
-          label={<FormName nome={formulario?.nome} />}
-          error={!!errors.nome}
-          helperText={errors.nome?.message}
-          id="step-1-nome"
-          {...register("nome")}
-        />
-      </Grid>
-      <Grid item xs={12} lg={6}>
-        <TextField
-          fullWidth
-          label={<FormName nome={formulario?.email} />}
-          error={!!errors.email}
-          helperText={errors.email?.message}
-          id="step-1-email"
-          {...register("email")}
-        />
-      </Grid>
-      <Grid item xs={12} lg={6}>
-        <TextField
-          fullWidth
-          label={<FormName nome={formulario?.rg} />}
-          error={!!errors.rg}
-          helperText={errors.rg?.message}
-          id="step-1-rg"
-          {...register("rg")}
-        />
-      </Grid>
-      <Grid item xs={12} lg={6}>
-        <TextField
-          fullWidth
-          label={<FormName nome={formulario?.cpf} />}
-          error={!!errors.cpf}
-          helperText={errors.cpf?.message}
-          id="step-1-cpf"
-          {...register("cpf")}
-        />
-      </Grid>
-      <Grid item xs={12} lg={6}>
-        <TextField
-          fullWidth
-          label={<FormName nome={formulario?.telefone} />}
-          error={!!errors.telefone}
-          helperText={errors.telefone?.message}
-          id="step-1-telefone"
-          {...register("telefone")}
-        />
-      </Grid>
-      <Grid item xs={12} lg={6}>
-        <TextField
-          fullWidth
-          label={<FormName nome={formulario?.dataNascimento} />}
-          type="date"
-          error={!!errors.dataNascimento}
-          helperText={errors.dataNascimento?.message}
-          id="step-1-dataNascimento"
-          InputLabelProps={{ shrink: true }}
-          {...register("dataNascimento")}
-        />
-      </Grid>
+	const FormName: React.FC<IFormQuery> = ({ nome }) => {
+		return <>{nome ? nome : <CircularProgress size={22} />}</>;
+	};
 
-      <Grid item xs={12} lg={6}>
-        <TextField
-          fullWidth
-          label={<FormName nome={formulario?.cidade} />}
-          error={!!errors.cidade}
-          helperText={errors.cidade?.message}
-          id="step-1-cidade"
-          {...register("cidade")}
-        />
-      </Grid>
-      <Grid item xs={12} lg={6}>
-        <FormControl fullWidth>
-          <InputLabel>Estado</InputLabel>
-          <Select
-            label={<FormName nome={formulario?.estado} />}
-            error={!!errors.estado}
-            defaultValue="AC"
-            id="step-1-estado"
-            {...register("estado")}
-          >
-            {estadosBrasileiros.map((estado) => (
-              <MenuItem key={estado.id} value={estado.sigla}>
-                {estado.nome}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </Grid>
+	return (
+		<FormGrid onSubmit={handleSubmit(onSubmit)}>
+			<Grid item xs={12}>
+				<TextField
+					fullWidth
+					label={<FormName nome={formulario?.nome} />}
+					error={!!errors.nome}
+					helperText={errors.nome?.message}
+					id="step-1-nome"
+					{...register("nome")}
+				/>
+			</Grid>
+			<Grid item xs={12} lg={6}>
+				<TextField
+					fullWidth
+					label={<FormName nome={formulario?.email} />}
+					error={!!errors.email}
+					helperText={errors.email?.message}
+					id="step-1-email"
+					{...register("email")}
+				/>
+			</Grid>
+			<Grid item xs={12} lg={6}>
+				<TextField
+					fullWidth
+					label={<FormName nome={formulario?.rg} />}
+					error={!!errors.rg}
+					helperText={errors.rg?.message}
+					id="step-1-rg"
+					{...register("rg")}
+				/>
+			</Grid>
+			<Grid item xs={6}>
+				<InputMask
+					mask="999.999.999-99"
+					maskChar=" "
+					{...register("cpf")}
+				>
+					{
+						// @ts-ignore
+						(inputProps) => (
+							<TextField
+								{...inputProps}
+								label={<FormName nome={formulario?.cpf} />}
+								variant="outlined"
+                helperText={errors.cpf?.message}
+								error={!!errors.cpf}
+								id="step-1-cpf"
+								sx={{
+									width: "100%",
+								}}
+							/>
+						)
+					}
+				</InputMask>
+			</Grid>
+			<Grid item xs={6} display="flex" flexDirection="column">
+				<InputMask
+					mask="(99)99999-9999"
+					maskChar=" "
+					{...register("telefone")}
+				>
+					{
+						// @ts-ignore
+						(inputProps) => (
+							<TextField
+								{...inputProps}
+								label={<FormName nome={formulario?.telefone} />}
+								variant="outlined"
+								error={!!errors.telefone}
+								helperText={errors.telefone?.message}
+								id="step-1-telefone"
+							/>
+						)
+					}
+				</InputMask>
+			</Grid>
+			<Grid item xs={12} lg={6}>
+				<TextField
+					fullWidth
+					label={<FormName nome={formulario?.dataNascimento} />}
+					type="date"
+					error={!!errors.dataNascimento}
+					helperText={errors.dataNascimento?.message}
+					id="step-1-dataNascimento"
+					InputLabelProps={{ shrink: true }}
+					{...register("dataNascimento")}
+				/>
+			</Grid>
 
-      <Grid item xs={12} lg={6}>
-        <Tooltip
-          title="Essa não é uma pergunta obrigatória, mas é importante para que possamos oferecer uma melhor experiência para você."
-          placement="top"
-          arrow
-        >
-          <FormControl fullWidth>
-            <InputLabel>
-              {<FormName nome={formulario?.neurodiversidade} />}
-            </InputLabel>
-            <Select
-              label={<FormName nome={formulario?.neurodiversidade} />}
-              defaultValue="F"
-              onChange={() => {
-                setNeurodiversidade(neurodiversidade === "F" ? "T" : "F");
-              }}
-              id="step-1-neurodiversidade"
-            >
-              <MenuItem value="F">Não</MenuItem>
-              <MenuItem value="T">Sim</MenuItem>
-            </Select>
-          </FormControl>
-        </Tooltip>
-      </Grid>
-      {neurodiversidade === "T" && (
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Qual neurodiversidade você possui?"
-            error={!!errors.neurodiversidade}
-            helperText={errors.neurodiversidade?.message}
-            id="step-1-neurodiversidade"
-            {...register("neurodiversidade")}
-          />
-        </Grid>
-      )}
+			<Grid item xs={12} lg={6}>
+				<TextField
+					fullWidth
+					label={<FormName nome={formulario?.cidade} />}
+					error={!!errors.cidade}
+					helperText={errors.cidade?.message}
+					id="step-1-cidade"
+					{...register("cidade")}
+				/>
+			</Grid>
+			<Grid item xs={12} lg={6}>
+				<FormControl fullWidth>
+					<InputLabel>Estado</InputLabel>
+					<Select
+						label={<FormName nome={formulario?.estado} />}
+						error={!!errors.estado}
+						defaultValue="AC"
+						id="step-1-estado"
+						{...register("estado")}
+					>
+						{estadosBrasileiros.map((estado) => (
+							<MenuItem key={estado.id} value={estado.sigla}>
+								{estado.nome}
+							</MenuItem>
+						))}
+					</Select>
+				</FormControl>
+			</Grid>
 
-      <Grid item xs={12}>
-        <Button type="submit" variant="contained" id="step-1-enviar">
-          Próximo
-        </Button>
-      </Grid>
-    </FormGrid>
-  );
+			<Grid item xs={12} lg={6}>
+				<Tooltip
+					title="Essa não é uma pergunta obrigatória, mas é importante para que possamos oferecer uma melhor experiência para você."
+					placement="top"
+					arrow
+				>
+					<FormControl fullWidth>
+						<InputLabel>
+							{<FormName nome={formulario?.neurodiversidade} />}
+						</InputLabel>
+						<Select
+							label={
+								<FormName nome={formulario?.neurodiversidade} />
+							}
+							defaultValue="F"
+							onChange={() => {
+								setNeurodiversidade(
+									neurodiversidade === "F" ? "T" : "F"
+								);
+							}}
+							id="step-1-neurodiversidade"
+						>
+							<MenuItem value="F">Não</MenuItem>
+							<MenuItem value="T">Sim</MenuItem>
+						</Select>
+					</FormControl>
+				</Tooltip>
+			</Grid>
+			{neurodiversidade === "T" && (
+				<Grid item xs={12}>
+					<TextField
+						fullWidth
+						label="Qual neurodiversidade você possui?"
+						error={!!errors.neurodiversidade}
+						helperText={errors.neurodiversidade?.message}
+						id="step-1-neurodiversidade"
+						{...register("neurodiversidade")}
+					/>
+				</Grid>
+			)}
+
+			<Grid item xs={12}>
+				<Button type="submit" variant="contained" id="step-1-enviar">
+					Próximo
+				</Button>
+			</Grid>
+		</FormGrid>
+	);
 };
--- src/shared/schemas/subscription.ts
+++ src/shared/schemas/subscription.ts
@@ -2,6 +2,7 @@ import * as Yup from "yup";
 
 export const stepOneSchema = Yup.object().shape({
   nome: Yup.string()
+    .required("O nome é obrigatório")
     .min(3, "O nome deve ter no mínimo 3 letras")
     .max(50, "O nome deve ter no máximo 50 letras")
     .matches(/^[a-zA-Z ]+$/, "O nome deve ter apenas letras e espaços")
@@ -11,29 +12,36 @@ export const stepOneSchema = Yup.object().shape({
         return nome && sobrenome;
       }
       return false;
-    })
-    .required("O nome é obrigatório"),
+    }),
+    
   email: Yup.string()
+    .required("O email é obrigatório")
     .min(3, "O email deve ter no mínimo 3 letras")
     .max(50, "O email deve ter no máximo 50 letras")
-    .email("O email deve ser um endereço de email válido")
-    .required("O email é obrigatório"),
+    .email("O email deve ser um endereço de email válido"),
+  rg: Yup.string()
+  .required("O RG é obrigatório")
+  .min(9, "O RG deve ter no mínimo 9 caracteres"),
+
   cpf: Yup.string()
-    .matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, "O CPF deve ser um número válido")
-    .required("O CPF é obrigatório"),
+    .required("O CPF é obrigatório")
+    .matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, "O CPF deve ser um número válido"),
+    
   telefone: Yup.string()
+    .required("O telefone é obrigatório")
     .matches(
       /^\(\d{2}\) \d{5}-\d{4}$/,
       "O telefone deve ser um número válido no formato (99) 99999-9999"
-    )
-    .required("O telefone é obrigatório"),
+    ),
+    
   cidade: Yup.string()
+    .required("A cidade é obrigatória")
     .min(3, "A cidade deve ter no mínimo 3 letras")
-    .max(50, "A cidade deve ter no máximo 50 letras")
-    .required("A cidade é obrigatória"),
+    .max(50, "A cidade deve ter no máximo 50 letras"),
+    
   dataNascimento: Yup.date()
-    .typeError("A data de nascimento deve ser uma data válida")
     .required("A data de nascimento é obrigatória")
+    .typeError("A data de nascimento deve ser uma data válida")
     .max(new Date(), "A data de nascimento deve ser anterior a data atual")
     .min(
       new Date(1900, 0, 1),

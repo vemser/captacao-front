e31a6d7824f656e135b4343a56dece626a911f41
Gustavo Scopinho <102439841+GustavoScopinho@users.noreply.github.com>
2023-01-09 16:45:59 -0300

    Add details

--- src/shared/features/api/candidato/candidatoSlice.ts
+++ src/shared/features/api/candidato/candidatoSlice.ts
@@ -1,94 +1,101 @@
-import { getToken } from "shared/utils/getToken";
-import { apiSlice } from "../";
+import { getToken } from 'shared/utils/getToken'
+import { apiSlice } from '../'
 import {
   CandidatoBody,
   CandidatoByNota,
   CandidatoByNotaBody,
   CandidatoResponse,
   Elemento,
-  UpdateNota, UpdateNotaParecerComportamental, UpdateNotaParecerTecnico,
-} from "./types";
+  UpdateNota,
+  UpdateNotaParecerComportamental,
+  UpdateNotaParecerTecnico
+} from './types'
 
 const candidatoSlice = apiSlice.injectEndpoints({
-  endpoints: (builder) => ({
+  endpoints: builder => ({
     postCandidato: builder.mutation<CandidatoResponse, CandidatoBody>({
-      query: (data) => ({
-        url: "candidato",
-        method: "POST",
+      query: data => ({
+        url: 'candidato',
+        method: 'POST',
         headers: {
-          Authorization: `Bearer ${getToken()}`,
+          Authorization: `Bearer ${getToken()}`
         },
-        body: data,
-      }),
+        body: data
+      })
     }),
     updateNota: builder.mutation<void, UpdateNota>({
-      query: (data) => ({
+      query: data => ({
         url: `candidato/nota-prova/${data.idCandidato}`,
-        method: "PUT",
+        method: 'PUT',
         headers: {
-          Authorization: `Bearer ${getToken()}`,
+          Authorization: `Bearer ${getToken()}`
         },
-        body: data.nota,
-      }),
+        body: data.nota
+      })
     }),
     getCandidatosByNota: builder.query<CandidatoByNota, CandidatoByNotaBody>({
-      query: (data) => ({
-        url: "candidato/find-by-nota",
-        method: "GET",
+      query: data => ({
+        url: 'candidato/find-by-nota',
+        method: 'GET',
         headers: {
-          Authorization: `Bearer ${getToken()}`,
+          Authorization: `Bearer ${getToken()}`
         },
         params: {
           pagina: data.pagina,
-          tamanho: data.tamanho,
-        },
-      }),
+          tamanho: data.tamanho
+        }
+      })
     }),
     getCandidatosByEmail: builder.mutation<Elemento, string>({
-      query: (data) => ({
-        url: "candidato/findbyemails",
-        method: "GET",
+      query: data => ({
+        url: 'candidato/findbyemails',
+        method: 'GET',
         headers: {
-          Authorization: `Bearer ${getToken()}`,
+          Authorization: `Bearer ${getToken()}`
         },
         params: {
-          email: data,
-        },
-      }),
+          email: data
+        }
+      })
     }),
     updateNotaParecerTecnico: builder.mutation<void, UpdateNotaParecerTecnico>({
-      query: (data) => ({
+      query: data => ({
         url: `candidato/nota-parecer-tecnico/${data.idCandidato}`,
         method: 'PUT',
         body: data,
         headers: {
-          Authorization: `Bearer ${getToken()}`,
+          Authorization: `Bearer ${getToken()}`
         },
         params: {
           idCandidato: data.idCandidato
         }
-      }),
+      })
     }),
-    updateNotaParecerComportamental: builder.mutation<void, UpdateNotaParecerComportamental>({
-      query: (data) => ({
+    updateNotaParecerComportamental: builder.mutation<
+      void,
+      UpdateNotaParecerComportamental
+    >({
+      query: data => ({
         url: `candidato/nota-comportamental/${data.idCandidato}`,
         method: 'PUT',
         body: data,
         headers: {
-          Authorization: `Bearer ${getToken()}`,
+          Authorization: `Bearer ${getToken()}`
         },
         params: {
           idCandidato: data.idCandidato
         }
-      }),
-    }),,
+      })
+    })
   }),
-  overrideExisting: false,
-});
+  overrideExisting: false
+})
 
 export const {
   usePostCandidatoMutation,
-  useUpdateNotaMutation, useUpdateNotaParecerTecnicoMutation, useUpdateNotaParecerComportamentalMutation ,
+  useUpdateNotaMutation,
+  useUpdateNotaParecerTecnicoMutation,
+  useUpdateNotaParecerComportamentalMutation,
   useGetCandidatosByNotaQuery,
-  useGetCandidatosByEmailMutation,
-} = candidatoSlice;
+  useGetCandidatosByEmailMutation
+} = candidatoSlice

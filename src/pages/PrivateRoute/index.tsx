b3dcafde85e21cc8fab6b437e0263387196bfa2d
Oliveiralucass <gf.oliveiralucas@gmail.com>
2023-01-11 15:12:30 -0300

    Adicionado auth

--- src/pages/PrivateRoute/index.tsx
+++ src/pages/PrivateRoute/index.tsx
@@ -7,31 +7,14 @@ import { useGetLoggedUserQuery } from "shared/features/api/usuario/authSlice";
 export const PrivateRoute = () => {
   const token = localStorage.getItem("token");
 
-  const { data: getLoggedUser } = useGetLoggedUserQuery();
-
-  const [ roles, setRoles ] = useState(false)
-
-  
-    if (getLoggedUser) {
-      if ((getLoggedUser?.cargos.filter(e => e.nome === 'ROLE_ADMIN').length > 0) || (getLoggedUser?.cargos.filter(e => e.nome === 'ROLE_GESTAO_DE_PESSOA').length > 0) || (getLoggedUser?.cargos.filter(e => e.nome === 'ROLE_INSTRUTOR').length > 0)) {
-        setRoles(true)
-
-      } else {
-        setRoles(false)
-
-      }
-    }
-
-  return token && roles == true ?(
+  return token ?(
     <DrawerContainer>
       <>
-      {toast.error('titeeeeeeeeeee')}
       <Outlet />
       </>
     </DrawerContainer>
   ) : (
     <>
-    {toast.error('DYBALAAAAAAA')}
     <Navigate to="/" />
     </>
   );

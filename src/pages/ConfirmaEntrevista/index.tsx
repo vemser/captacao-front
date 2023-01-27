97b4d320aa32122c03b9b35e02302af369de497a
Gicelly Oliveira <gicelly.oliveira@gmail.com>
2023-01-27 16:16:18 -0300

    configuração de página de confirmação de entrevista

--- src/pages/ConfirmaEntrevista/index.tsx
+++ src/pages/ConfirmaEntrevista/index.tsx
@@ -1,7 +1,9 @@
+import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useConfirmaEntrevistaMutation } from "shared/features/api/entrevista/entrevistaSlice";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 export const ConfirmaEntrevista: React.FC = () => {
 	let [searchParams] = useSearchParams();
@@ -17,16 +19,29 @@ export const ConfirmaEntrevista: React.FC = () => {
 		<Box
 			display="flex"
 			alignItems="center"
+			flexDirection="column"
 			justifyContent="center"
 			width="100vw"
 			height="100vh"
 			bgcolor="#1e62fe"
+			gap={4}
 		>
 			{/* eslint-disable-next-line jsx-a11y/alt-text */}
+
 			<img
 				src={require("../../assets/logo-white.webp")}
-				style={{ width: "50%" }}
+				style={{ width: "10%" }}
 			/>
+			<Typography display= "flex" gap={2}
+    alignItems="center" fontSize="1.5rem" variant="h3" color="#ffff">
+				Entrevista confirmada com sucesso{" "}
+				<CheckCircleIcon
+					sx={{
+            alignItems:"center",
+						color: "#90EE90",
+					}}
+				/>
+			</Typography>
 		</Box>
 	);
 };

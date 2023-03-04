77c408e918d3469ef9ef60cb2ce252f47a9b7e49
Maicon Gerardi <maicon.gerardi@gmail.com>
2023-03-04 01:00:23 -0300

    feat: excluir inscrição

--- src/shared/features/api/export.ts
+++ src/shared/features/api/export.ts
@@ -47,7 +47,7 @@ export const getCandidatosExcel = async () => {
         const url = window.URL.createObjectURL(new Blob([response.data]));
         const link = document.createElement('a');
         link.href = url;
-        link.setAttribute('download', 'candidatos.xlsx');
+        link.setAttribute('download', 'candidatos.csv');
         document.body.appendChild(link);
         link.click();
     }).finally(() => {
